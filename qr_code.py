cd3ceaff25851c57f8aa67a61eb75c1fb90856e0
Matthew Feickert <matthew.feickert@cern.ch>
2026-02-15 03:26:11 -0700

    Update QR code for poster

--- qr_code.py
+++ qr_code.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
 import qrcode
 import qrcode.image.svg
 
 
 def main():
-    url = 'https://diana-hep.org/pyhf'
+    url = "https://github.com/CHTC/templates-GPUs"
     factory = qrcode.image.svg.SvgFillImage
     img = qrcode.make(url, image_factory=factory)
-    img.save('figures/qr_code.svg')
+
+    figures_dir = Path.cwd() / "figures"
+    figures_dir.mkdir(exist_ok=True)
+    img.save(figures_dir / "qr_code.svg")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
